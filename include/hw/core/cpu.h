773c485b97a085752295d3cfb1d0cb0311db7728
Robert Foley <robert.foley@linaro.org>
2020-07-09 09:18:59 -0400

    Introduce GIC lock, per CPU PMU locking and per CPU locking for the timer related registers.

--- include/hw/core/cpu.h
+++ include/hw/core/cpu.h
@@ -541,11 +541,8 @@ static inline uint32_t cpu_interrupt_request(CPUState *cpu)
 {
     return atomic_read(&cpu->interrupt_request);
 }
-static inline uint32_t cpu_interrupt_request_or(CPUState *cpu, uint32_t mask)
+static inline void cpu_interrupt_request_or(CPUState *cpu, uint32_t mask)
 {
-#if 1
-    return atomic_fetch_or(&cpu->interrupt_request, mask);
-#else
     if (cpu_mutex_locked(cpu)) {
         atomic_set(&cpu->interrupt_request, cpu->interrupt_request | mask);
         return;
@@ -553,7 +550,6 @@ static inline uint32_t cpu_interrupt_request_or(CPUState *cpu, uint32_t mask)
     cpu_mutex_lock(cpu);
     atomic_set(&cpu->interrupt_request, cpu->interrupt_request | mask);
     cpu_mutex_unlock(cpu);
-#endif    
 }
 
 static inline void cpu_interrupt_request_set(CPUState *cpu, uint32_t val)
